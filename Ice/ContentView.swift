56fc20d6c49694e3c1ed22804ad4bc6fc5c9c5ea
Jordan Baird <jordanbaird.dev@gmail.com>
2023-07-30 05:20:14 -0600

    Initial commit

---
+++ Ice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ice
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ice/IceApp.swift
@@ -0,0 +1,15 @@
+//
+//  IceApp.swift
+//  Ice
+//
+
+import SwiftUI
+
+@main
+struct IceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

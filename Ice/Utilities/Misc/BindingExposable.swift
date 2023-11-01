0812f99d3e8dc44c0c8d0ccc01b6467e0c9f65ab
Jordan Baird <jordanbaird.dev@gmail.com>
2023-11-01 06:38:08 -0600

    Update BindingExposable

--- Ice/Utilities/Misc/BindingExposable.swift
+++ Ice/Utilities/Misc/BindingExposable.swift
@@ -6,36 +6,35 @@
 import SwiftUI
 
 /// A type that acts as a lens that exposes bindings to the
-/// writable properties on a base object.
+/// writable properties of a base object.
 @dynamicMemberLookup
-struct ExposedBindings<Base> {
+struct ExposedBindings<Base: BindingExposable> {
     /// The base object whose bindings are exposed.
-    let base: Base
+    fileprivate let base: Base
 
-    /// Returns a binding to the writable property at the given
-    /// key path.
-    subscript<Value>(
-        dynamicMember keyPath: ReferenceWritableKeyPath<Base, Value>
-    ) -> Binding<Value> {
+    /// Returns a binding to the property at the given key path.
+    subscript<Value>(dynamicMember keyPath: ReferenceWritableKeyPath<Base, Value>) -> Binding<Value> {
         Binding(
-            get: {
-                base[keyPath: keyPath]
-            },
-            set: { newValue in
-                base[keyPath: keyPath] = newValue
-            }
+            get: { base[keyPath: keyPath] },
+            set: { base[keyPath: keyPath] = $0 }
         )
+    }
+
+    /// Returns a lens that exposes the bindings of the object
+    /// at the given key path.
+    subscript<T: BindingExposable>(dynamicMember keyPath: KeyPath<Base, T>) -> ExposedBindings<T> {
+        ExposedBindings<T>(base: base[keyPath: keyPath])
     }
 }
 
 /// A type that exposes its writable properties as bindings.
 protocol BindingExposable {
     /// A type that acts as a lens that exposes bindings to the
-    /// writable properties on this type.
+    /// writable properties of this type.
     typealias Bindings = ExposedBindings<Self>
 
     /// A lens that exposes bindings to the writable properties
-    /// on this instance.
+    /// of this instance.
     var bindings: Bindings { get }
 }
 

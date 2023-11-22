8b6adb61eb865998289b5cebef83b7b3ca880f00
Jordan Baird <jordanbaird.dev@gmail.com>
2023-11-22 05:52:56 -0700

    Sort defaults

--- Ice/Utilities/Misc/Defaults.swift
+++ Ice/Utilities/Misc/Defaults.swift
@@ -5,15 +5,15 @@
 
 /// A namespace for `UserDefaults` keys.
 enum Defaults {
-    static let iceIcon = "IceIcon"
     static let customIceIconIsTemplate = "CustomIceIconIsTemplate"
-    static let menuBarHasShadow = "MenuBarHasShadow"
+    static let iceIcon = "IceIcon"
     static let menuBarHasBorder = "MenuBarHasBorder"
+    static let menuBarHasShadow = "MenuBarHasShadow"
     static let menuBarBorderColor = "MenuBarBorderColor"
     static let menuBarBorderWidth = "MenuBarBorderWidth"
-    static let menuBarTintKind = "MenuBarTintKind"
     static let menuBarTintColor = "MenuBarTintColor"
     static let menuBarTintGradient = "MenuBarTintGradient"
+    static let menuBarTintKind = "MenuBarTintKind"
     static let secondaryActionModifier = "SecondaryActionModifier"
     static let sections = "Sections"
 }

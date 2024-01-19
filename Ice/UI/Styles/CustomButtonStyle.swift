138355ed60daa4d2d5159103ade8eba6e5189091
Jordan Baird <jordanbaird.dev@gmail.com>
2024-01-19 07:18:54 -0700

    Simplify `CustomButtonStyle`
    Improve documentation
    
    Remove dumb jokes

--- Ice/UI/Styles/CustomButtonStyle.swift
+++ Ice/UI/Styles/CustomButtonStyle.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 /// A custom button style to use in the app's interface.
 struct CustomButtonStyle: PrimitiveButtonStyle {
-    /// A custom view that prevents mouse down messages from
+    /// Custom view that prevents mouse down messages from
     /// passing through to the button's window.
     private struct MouseDownInterceptor: NSViewRepresentable {
         private class Represented: NSView {
@@ -17,7 +17,7 @@ struct CustomButtonStyle: PrimitiveButtonStyle {
         }
 
         func makeNSView(context: Context) -> NSView {
-            Represented()
+            return Represented()
         }
 
         func updateNSView(_: NSView, context: Context) { }
@@ -31,42 +31,32 @@ struct CustomButtonStyle: PrimitiveButtonStyle {
 
         func path(in rect: CGRect) -> Path {
             if shape.flattenedEdges == .all {
-                // fast path (pun not intended)
+                // fast path
                 return Path(rect)
             }
             var path = Path(roundedRect: rect, cornerRadius: cornerRadius)
             if shape.flattenedEdges.isEmpty {
-                // fast path MkII
+                // fast path
                 return path
             }
             for edge in Edge.allCases where shape.flattenedEdges.contains(Edge.Set(edge)) {
-                flatten(edge: edge, of: &path)
+                let slice = path.boundingRect.divided(atDistance: cornerRadius, from: edge.cgRectEdge).slice
+                path = path.union(Path(slice))
             }
             return path
         }
-
-        func flatten(edge: Edge, of path: inout Path) {
-            let (rect, distance, edge) = (path.boundingRect, cornerRadius, edge.cgRectEdge)
-            let slice = rect.divided(atDistance: distance, from: edge).slice
-            path = Path(path.cgPath.union(Path(slice).cgPath))
-        }
     }
 
-    @Environment(\.customButtonConfiguration.bezelOpacity) private var bezelOpacity
-    @Environment(\.customButtonConfiguration.isHighlighted) private var isHighlighted
-    @Environment(\.customButtonConfiguration.labelForegroundColor) private var labelForegroundColor
-    @Environment(\.customButtonConfiguration.shape) private var shape
     @Environment(\.controlSize) private var controlSize
-    @State private var horizontalPadding: CGFloat = 7
-    @State private var verticalPadding: CGFloat = 2
-    @State private var isPressed = false
+    @Environment(\.customButtonConfiguration) private var customButtonConfiguration
     @State private var frame = CGRect.zero
+    @State private var isPressed = false
+    @State private var padding = EdgeInsets(top: 2, leading: 7, bottom: 2, trailing: 7)
 
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
-            .foregroundStyle(labelForegroundColor)
-            .padding(.horizontal, horizontalPadding)
-            .padding(.vertical, verticalPadding)
+            .foregroundStyle(customButtonConfiguration.labelForegroundColor)
+            .padding(padding)
             .baselineOffset(1)
             .transformEnvironment(\.font) { font in
                 if font == nil {
@@ -75,14 +65,12 @@ struct CustomButtonStyle: PrimitiveButtonStyle {
             }
             .background {
                 Color.primary
-                    .opacity(isHighlighted ? 0.2 : 0)
+                    .opacity(customButtonConfiguration.isHighlighted ? 0.2 : 0)
                     .blendMode(.overlay)
                     .background(isPressed ? .tertiary : .quaternary)
-                    .background {
-                        MouseDownInterceptor()
-                    }
-                    .clipShape(ClipShape(cornerRadius: 5, shape: shape))
-                    .opacity(bezelOpacity)
+                    .background { MouseDownInterceptor() }
+                    .clipShape(ClipShape(cornerRadius: 5, shape: customButtonConfiguration.shape))
+                    .opacity(customButtonConfiguration.bezelOpacity)
             }
             .simultaneousGesture(
                 DragGesture(minimumDistance: 0)
@@ -96,30 +84,27 @@ struct CustomButtonStyle: PrimitiveButtonStyle {
                         }
                     }
             )
-            .onFrameChange(update: $frame)
             .onAppear {
                 switch controlSize {
                 case .mini:
-                    horizontalPadding = 2
-                    verticalPadding = 0
+                    padding = EdgeInsets(top: 0, leading: 2, bottom: 0, trailing: 2)
                 case .small:
-                    horizontalPadding = 4
-                    verticalPadding = 1
+                    padding = EdgeInsets(top: 1, leading: 4, bottom: 1, trailing: 4)
                 case .regular:
-                    horizontalPadding = 7
-                    verticalPadding = 2
+                    padding = EdgeInsets(top: 2, leading: 7, bottom: 2, trailing: 7)
                 case .large:
-                    horizontalPadding = 8
-                    verticalPadding = 6
+                    padding = EdgeInsets(top: 6, leading: 8, bottom: 6, trailing: 8)
                 case .extraLarge:
-                    horizontalPadding = 12
-                    verticalPadding = 8
+                    padding = EdgeInsets(top: 8, leading: 12, bottom: 8, trailing: 12)
                 @unknown default:
                     break
                 }
             }
+            .onFrameChange(update: $frame)
     }
 }
+
+// MARK: - PrimitiveButtonStyle where Self == CustomButtonStyle
 
 extension PrimitiveButtonStyle where Self == CustomButtonStyle {
     /// A custom button style to use in the app's interface.
@@ -131,19 +116,19 @@ extension PrimitiveButtonStyle where Self == CustomButtonStyle {
 // MARK: - CustomButtonConfiguration
 
 /// A configuration that determines the appearance and behavior
-/// of a custom button.
+/// of buttons using the `custom` button style.
 struct CustomButtonConfiguration {
-    /// A configuration that determines the shape of a custom button.
+    /// A configuration that determines the shape of the button.
     struct ButtonShape {
-        /// The flattened edges of the buttons that use this configuration.
+        /// The flattened edges of the button.
         var flattenedEdges: Edge.Set = []
 
-        /// A configuration for a settings button with the shape of a
-        /// leading segment in a segmented control.
+        /// A configuration for a button with the shape of a leading
+        /// segment in a segmented control.
         static let leadingSegment = ButtonShape(flattenedEdges: .trailing)
 
-        /// A configuration for a settings button with the shape of a
-        /// trailing segment in a segmented control.
+        /// A configuration for a button with the shape of a trailing
+        /// segment in a segmented control.
         static let trailingSegment = ButtonShape(flattenedEdges: .leading)
     }
 
@@ -176,8 +161,8 @@ private extension EnvironmentValues {
 }
 
 extension View {
-    /// Configures the properties of buttons in this view using
-    /// the given closure.
+    /// Configures the properties of buttons in this view that use
+    /// the `custom` button style.
     ///
     /// - Parameter configure: A closure that updates the current
     ///   environment's custom button configuration with new values.

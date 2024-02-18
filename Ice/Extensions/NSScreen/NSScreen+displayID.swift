cd6080efc539321725e88714542e7340ba62ecc5
Jordan Baird <jordanbaird.dev@gmail.com>
2024-02-17 23:36:02 -0700

    Update NSScreen+displayID.swift

--- Ice/Extensions/NSScreen/NSScreen+displayID.swift
+++ Ice/Extensions/NSScreen/NSScreen+displayID.swift
@@ -8,8 +8,11 @@ import Cocoa
 extension NSScreen {
     /// The display identifier of the screen.
     var displayID: CGDirectDisplayID {
-        // deviceDescription is guaranteed to always have an NSScreenNumber key, so a force unwrap here is okay
-        let screenNumber = deviceDescription[NSDeviceDescriptionKey("NSScreenNumber")]! // swiftlint:disable:this force_unwrapping
-        return screenNumber as! CGDirectDisplayID // swiftlint:disable:this force_cast
+        // deviceDescription dictionary is guaranteed to always have an "NSScreenNumber"
+        // key with a CGDirectDisplayID as its value, so a force unwrap and cast is okay
+        // swiftlint:disable:next force_unwrapping
+        let screenNumber = deviceDescription[NSDeviceDescriptionKey("NSScreenNumber")]!
+        // swiftlint:disable:next force_cast
+        return screenNumber as! CGDirectDisplayID
     }
 }

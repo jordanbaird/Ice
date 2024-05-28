0cbffb28577ec0834cca04b3dd2a06f8c0c4d0f1
Jordan Baird <jordanbaird.dev@gmail.com>
2024-05-27 20:12:24 -0600

    Use copyright string from bundle

--- Ice/Utilities/Constants.swift
+++ Ice/Utilities/Constants.swift
@@ -6,12 +6,13 @@
 import Foundation
 
 enum Constants {
+    // swiftlint:disable force_unwrapping
     /// The version string in the app's bundle.
-    static let appVersion = Bundle.main.versionString! // swiftlint:disable:this force_unwrapping
+    static let appVersion = Bundle.main.versionString!
 
-    /// A user-readable copyright string formatted with non-breaking
-    /// spaces for display in the user interface.
-    static let copyright: String = "Copyright\u{00A0}©\u{00A0}2024 Jordan\u{00A0}Baird. All\u{00A0}Rights\u{00A0}Reserved." // U+00A0 'NO-BREAK SPACE'
+    /// The user-readable copyright string in the app's bundle.
+    static let copyright = Bundle.main.copyrightString!
+    // swiftlint:enable force_unwrapping
 
     /// The identifier for the settings window.
     static let settingsWindowID = "SettingsWindow"

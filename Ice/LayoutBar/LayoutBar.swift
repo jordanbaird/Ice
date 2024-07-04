28f022cb6552e29b5e9360a559f8963133ab2e6c
Jordan Baird <jordanbaird.dev@gmail.com>
2024-07-03 19:43:22 -0600

    Tweak wording

--- Ice/LayoutBar/LayoutBar.swift
+++ Ice/LayoutBar/LayoutBar.swift
@@ -45,7 +45,7 @@ struct LayoutBar: View {
     @ViewBuilder
     private var conditionalBody: some View {
         if appState.imageCache.cacheFailed(for: section.name) {
-            Text("Unable to display menu bar items. Try switching spaces.")
+            Text("Unable to display menu bar items")
                 .foregroundStyle(menuBarManager.averageColorInfo?.color.brightness ?? 0 > 0.67 ? .black : .white)
         } else {
             Representable(appState: appState, section: section, spacing: spacing)

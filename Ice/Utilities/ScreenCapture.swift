38d344f392888bf9a419815d31a660774a2ea31c
Jordan Baird <jordanbaird.dev@gmail.com>
2025-09-18 08:33:17 -0600

    Update screen capture
    Revert to using a direct call to the `CGImage` initializer, rather than calling it through a static protocol method (this was originally done to suppress the deprecation warning, but it seems to cause screen capture to fail for some users).

--- Ice/Utilities/ScreenCapture.swift
+++ Ice/Utilities/ScreenCapture.swift
@@ -3,8 +3,6 @@
 //  Ice
 //
 
-// MARK: - ScreenCapture
-
 import CoreGraphics
 import ScreenCaptureKit
 
@@ -13,7 +11,8 @@ enum ScreenCapture {
 
     // MARK: Permissions
 
-    /// Returns a Boolean value that indicates whether the app has screen capture permissions.
+    /// Returns a Boolean value that indicates whether the app has screen
+    /// capture permissions.
     static func checkPermissions() -> Bool {
         for windowID in Bridging.getMenuBarWindowList(option: [.itemsOnly, .activeSpace]) {
             guard
@@ -24,24 +23,24 @@ enum ScreenCapture {
             }
             return window.title != nil
         }
-        // CGPreflightScreenCaptureAccess() only returns an initial value, but we can
-        // use it as a fallback.
+        // CGPreflightScreenCaptureAccess() only returns an initial value,
+        // but we can use it as a fallback.
         return CGPreflightScreenCaptureAccess()
     }
 
-    /// Returns a Boolean value that indicates whether the app has screen capture permissions.
+    /// Returns a Boolean value that indicates whether the app has screen
+    /// capture permissions.
     ///
-    /// This function caches its initial result and returns it on subsequent calls. Pass `true`
-    /// to the `reset` parameter to replace the cached result with a newly computed value.
+    /// This function caches its initial result and returns it on subsequent
+    /// calls. Pass `true` to the `reset` parameter to replace the cached
+    /// result with a newly computed value.
     static func cachedCheckPermissions(reset: Bool = false) -> Bool {
         enum Context {
             static var cachedResult: Bool?
         }
-
         if !reset, let result = Context.cachedResult {
             return result
         }
-
         let result = checkPermissions()
         Context.cachedResult = result
         return result
@@ -50,9 +49,10 @@ enum ScreenCapture {
     /// Requests screen capture permissions.
     static func requestPermissions() {
         if #available(macOS 15.0, *) {
-            // TODO: Find out if we still need this.
-            // CGRequestScreenCaptureAccess() is broken on macOS 15. SCShareableContent requires
-            // screen capture permissions, and triggers a request if the user doesn't have them.
+            // CGRequestScreenCaptureAccess() is broken on macOS 15. We can
+            // try accessing SCShareableContent to trigger a request if the
+            // user doesn't have permissions.
+            // TODO: Find out if we still need this as of macOS 26.
             SCShareableContent.getWithCompletionHandler { _, _ in }
         } else {
             CGRequestScreenCaptureAccess()
@@ -61,56 +61,34 @@ enum ScreenCapture {
 
     // MARK: Capture Window(s)
 
-    /// Queue for screen capture operations.
-    private static let captureQueue = DispatchQueue(label: "ScreenCapture.captureQueue", qos: .userInteractive)
-
     /// Captures a composite image of an array of windows.
     ///
-    /// The windows are composited from front to back, according to the order of the `windowIDs`
-    /// parameter.
+    /// The windows are composited from front to back, according to the order
+    /// of the `windowIDs` parameter.
     ///
     /// - Parameters:
     ///   - windowIDs: The identifiers of the windows to capture.
-    ///   - screenBounds: The bounds to capture, specified in screen coordinates. Pass `nil` to
-    ///     capture the minimum rectangle that encloses the windows.
+    ///   - screenBounds: The bounds to capture, specified in screen coordinates.
+    ///     Pass `nil` to capture the minimum rectangle that encloses the windows.
     ///   - option: Options that specify which parts of the windows are captured.
     static func captureWindows(with windowIDs: [CGWindowID], screenBounds: CGRect? = nil, option: CGWindowImageOption = []) -> CGImage? {
         guard let array = Bridging.createCGWindowArray(with: windowIDs) else {
             return nil
         }
         let bounds = screenBounds ?? .null
-        return captureQueue.sync {
-            CGImage.createWindowListImageFromArray(screenBounds: bounds, windowArray: array, imageOption: option)
-        }
+        // ScreenCaptureKit doesn't support capturing images of offscreen menu bar
+        // items, so we unfortunately have to use the deprecated CGWindowList API.
+        return CGImage(windowListFromArrayScreenBounds: bounds, windowArray: array, imageOption: option)
     }
 
     /// Captures an image of a window.
     ///
     /// - Parameters:
     ///   - windowID: The identifier of the window to capture.
-    ///   - screenBounds: The bounds to capture, specified in screen coordinates. Pass `nil` to
-    ///     capture the minimum rectangle that encloses the window.
+    ///   - screenBounds: The bounds to capture, specified in screen coordinates.
+    ///     Pass `nil` to capture the minimum rectangle that encloses the window.
     ///   - option: Options that specify which parts of the window are captured.
     static func captureWindow(with windowID: CGWindowID, screenBounds: CGRect? = nil, option: CGWindowImageOption = []) -> CGImage? {
         captureWindows(with: [windowID], screenBounds: screenBounds, option: option)
     }
 }
-
-// MARK: - WindowListImage Helper
-
-/// A protocol to suppress warnings for the deprecated CGWindowList screen capture APIs.
-///
-/// ScreenCaptureKit doesn't support capturing composite images of offscreen menu bar items.
-/// This should be replaced once it does.
-private protocol WindowListImage {
-    init?(windowListFromArrayScreenBounds: CGRect, windowArray: CFArray, imageOption: CGWindowImageOption)
-}
-
-private extension WindowListImage {
-    @inline(__always) // Ensure a direct call to the initializer.
-    static func createWindowListImageFromArray(screenBounds: CGRect, windowArray: CFArray, imageOption: CGWindowImageOption) -> Self? {
-        Self(windowListFromArrayScreenBounds: screenBounds, windowArray: windowArray, imageOption: imageOption)
-    }
-}
-
-extension CGImage: WindowListImage { }
